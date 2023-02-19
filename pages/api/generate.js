9e768ef34fb3906ba1f29084dc9a69063f390d28
Patatatat <andres.guluarte@gmail.com>
2023-02-19 14:21:51 -0600

    inage doesn't generate

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,10 +1,18 @@
+const bufferToBase64 = (buffer) => {
+  let arr = new Uint8Array(buffer);
+  const base64 = btoa(
+    arr.reduce((data, byte) => data + String.fromCharCode(byte), '')
+  )
+  return `data:image/png;base64,${base64}`;
+};
+
 const generateAction = async (req, res) => {
   console.log("Received request");
   
   const input = JSON.parse(req.body).input;
 
   const response = await fetch(
-    `https://api-inference.huggingface.co/models/buildspace/ai-avatar-generator`,
+    `https://huggingface.co/Patatatat/sd-1-5-andres`,
     {
       headers: {
         Authorization: `Bearer ${process.env.HF_AUTH_KEY}`,
@@ -19,7 +27,8 @@ const generateAction = async (req, res) => {
 
   if (response.ok) {
     const buffer = await response.arrayBuffer();
-    res.status(200).json({ image: buffer });
+    const base64 = bufferToBase64(buffer);
+    res.status(200).json({ image: base64 });
   } else if (response.status === 503) {
     const json = await response.json();
     res.status(503).json(json);
--- pages/index.js
+++ pages/index.js
@@ -4,23 +4,28 @@ import Image from "next/image";
 import buildspaceLogo from "../assets/buildspace-logo.png";
 
 const Home = () => {
+  // Create state property
   const maxRetries = 20;
   const [input, setInput] = useState("");
   const [img, setImg] = useState("");
+
+  // Numbers of retries
   const [retry, setRetry] = useState(0);
+  // Number of retries left
   const [retryCount, setRetryCount] = useState(maxRetries);
   const [isGenerating, setIsGenerating] = useState(false);
+  const [finalPrompt, setFinalPrompt] = useState("");
 
+  // Add this function
   const onChange = (event) => {
     setInput(event.target.value);
   };
 
   const generateAction = async () => {
-    console.log("Generating please wait...");
+    console.log("Generating...");
 
     if (isGenerating && retry === 0) return;
 
-    // Set loading has started
     setIsGenerating(true);
 
     if (retry > 0) {
@@ -35,18 +40,19 @@ const Home = () => {
       setRetry(0);
     }
 
-    const response = await fetch("./api/generate.js", {
+    const finalInput = input.replace(/raza/gi, "abraza");
+
+    const response = await fetch("/api/generate", {
       method: "POST",
       headers: {
         "Content-Type": "image/jpeg",
       },
-      body: JSON.stringify({ input }),
+      body: JSON.stringify({ finalInput }),
     });
 
     const data = await response.json();
 
     if (response.status === 503) {
-      // Set the estimated_time property in state
       setRetry(data.estimated_time);
       return;
     }
@@ -56,38 +62,43 @@ const Home = () => {
       setIsGenerating(false);
       return;
     }
+
+    // Set final prompt here
+    setFinalPrompt(input);
+    // Remove content from input box
+    setInput("");
     setImg(data.image);
     setIsGenerating(false);
+  };
 
-    const sleep = (ms) => {
-      return new Promise((resolve) => {
-        setTimeout(resolve, ms);
-      });
-    };
+  const sleep = (ms) => {
+    return new Promise((resolve) => {
+      setTimeout(resolve, ms);
+    });
+  };
 
-    useEffect(() => {
-      const runRetry = async () => {
-        if (retryCount === 0) {
-          console.log(
-            `Model still loading after ${maxRetries} retries. Try request again in 5 minutes.`
-          );
-          setRetryCount(maxRetries);
-          return;
-        }
-
-        console.log(`Trying again in ${retry} seconds.`);
-
-        await sleep(retry * 1000);
-        await generateAction();
-      };
-
-      if (retry === 0) {
+  useEffect(() => {
+    const runRetry = async () => {
+      if (retryCount === 0) {
+        console.log(
+          `Model still loading after ${maxRetries} retries. Try request again in 5 minutes.`
+        );
+        setRetryCount(maxRetries);
         return;
       }
 
-      runRetry();
-    }, [retry]);
-  };
+      console.log(`Trying again in ${retry} seconds.`);
+
+      await sleep(retry * 1000);
+      await generateAction();
+    };
+
+    if (retry === 0) {
+      return;
+    }
+
+    runRetry();
+  }, [retry]);
 
   return (
     <div className="root">
@@ -127,6 +138,12 @@ const Home = () => {
             </div>
           </div>
         </div>
+        {img && (
+          <div className="output-content">
+            <Image src={img} width={512} height={512} alt={finalPrompt} />
+            <p>{finalPrompt}</p>
+          </div>
+        )}
       </div>
       <div className="badge-container grow">
         <a
